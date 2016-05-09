53182b516118028e2ca5e57fe267000d9ba1474f
alpipego <alpipego@gmail.com>
2016-05-09 16:30:16 +0200

    removed upload dir url/path resolution

--- dynamic-attachment-resizer.php
+++ dynamic-attachment-resizer.php
@@ -11,26 +11,3 @@ use Alpipego\WpLib\Autoload;
  */
 
 new Autoload(__DIR__, 'Alpipego');
-
-add_filter('upload_dir', function($uploads) {
-	$uploads['path'] = realpath($uploads['path']);
-	$uploads['basedir'] = realpath($uploads['basedir']);
-
-	while (strpos($uploads['url'], '/./')) {
-		$uploads['url'] = preg_replace( '%(?:/\.{1}/)%', '/', $uploads['url'] );
-	}
-
-	while (strpos($uploads['baseurl'], '/./')) {
-		$uploads['baseurl'] = preg_replace( '%(?:/\.{1}/)%', '/', $uploads['baseurl'] );
-	}
-
-	while (strpos($uploads['url'], '/../')) {
-		$uploads['url'] = preg_replace( '%(?:([^/]+?)/\.{2}/)%', '', $uploads['url']);
-	}
-
-	while (strpos($uploads['baseurl'], '/../')) {
-		$uploads['baseurl'] = preg_replace( '%(?:([^/]+?)/\.{2}/)%', '', $uploads['baseurl']);
-	}
-
-	return $uploads;
-});
