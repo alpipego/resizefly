400b27fbcf0388edf7bef6257ea8a892c168b24f
alpipego <alpipego@gmail.com>
2016-06-15 19:37:21 +0200

    implement php version check, deactivate if <= 5.4

--- resizefly.php
+++ resizefly.php
@@ -1,21 +1,24 @@
 <?php
 
-namespace Alpipego\Resizefly;
-
 /**
  * Plugin Name: Resizefly
  * Description: Dynamically resize your images on the fly
  * Plugin URI:  http://resizefly.com/
  * Version:     1.1.4
- * Author:      Alex
+ * Author:      alpipego
  * Author URI:  http://alpipego.com/
+ * Text Domain: resizefly
  */
+
+require_once dirname(__FILE__) . '/version-check.php';
 
 use Alpipego\Resizefly\Image\Editor as ImageEditor;
 use Alpipego\Resizefly\Image\Handler as ImageHandler;
 use Alpipego\Resizefly\Image\Image;
 use Alpipego\Resizefly\Image\Stream;
 use Alpipego\Resizefly\Upload\Fake;
+use Alpipego\Resizefly\Plugin;
+use Alpipego\Resizefly\Autoload;
 
 require_once __DIR__ . '/src/Autoload.php';
 new Autoload();
@@ -23,8 +26,10 @@ new Autoload();
 \add_action( 'plugins_loaded', function () {
 	$plugin = new Plugin();
 
-	$plugin['path']    = realpath( \plugin_dir_path( __FILE__ ) ) . DIRECTORY_SEPARATOR;
-	$plugin['url']     = \plugin_dir_url( __FILE__ );
+	$plugin->loadTextdomain( __DIR__ );
+
+	$plugin['path']    = realpath( plugin_dir_path( __FILE__ ) ) . DIRECTORY_SEPARATOR;
+	$plugin['url']     = plugin_dir_url( __FILE__ );
 	$plugin['version'] = '1.1.4';
 
 	$plugin['addons'] = apply_filters( 'resizefly_addons', [ ] );
@@ -42,7 +47,7 @@ new Autoload();
 	$plugin->run();
 
 
-	\add_action( 'template_redirect', function () use ( $plugin ) {
+	add_action( 'template_redirect', function () use ( $plugin ) {
 		if ( ! is_404() ) {
 			return;
 		}
@@ -52,14 +57,14 @@ new Autoload();
 
 			// get the correct path ("regardless" of WordPress installation path etc)
 			$plugin['image'] = function ( $plugin ) {
-				return new Image( $plugin['requested_file'], \wp_upload_dir( null, false ), \get_bloginfo( 'url' ) );
+				return new Image( $plugin['requested_file'], wp_upload_dir( null, false ), get_bloginfo( 'url' ) );
 			};
 
 			// get wp image editor and handle errors
-			$plugin['wp_image_editor'] = \wp_get_image_editor( $plugin['image']->original );
-			if ( ! file_exists( $plugin['image']->original ) || \is_wp_error( $plugin['wp_image_editor'] ) ) {
-				\status_header( '404' );
-				@include_once \get_404_template();
+			$plugin['wp_image_editor'] = wp_get_image_editor( $plugin['image']->original );
+			if ( ! file_exists( $plugin['image']->original ) || is_wp_error( $plugin['wp_image_editor'] ) ) {
+				status_header( '404' );
+				@include_once get_404_template();
 
 				exit;
 			}
@@ -76,7 +81,7 @@ new Autoload();
 
 			// output stream the resized image
 			$plugin['output'] = function ( $plugin ) {
-				return new Stream( \wp_get_image_editor( $plugin['image_handler']->file ) );
+				return new Stream( wp_get_image_editor( $plugin['image_handler']->file ) );
 			};
 
 			$plugin->run();
