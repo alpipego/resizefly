53e819bd6636ce55cbae39ab85a794a549d53e48
alpipego <alpipego@gmail.com>
2016-06-20 16:37:34 +0200

    added resized path filter

--- src/Image/Handler.php
+++ src/Image/Handler.php
@@ -8,58 +8,64 @@
 
 namespace Alpipego\Resizefly\Image;
 
+use Exception;
 
 class Handler {
-    protected $image;
-    protected $editor;
-    protected $aspect;
-    public $file;
+	public $file;
+	protected $image;
+	protected $editor;
+	protected $aspect;
 
-    public function __construct( Image $image, Editor $editor ) {
-        $this->image  = $image;
-        $this->editor = $editor;
-    }
+	public function __construct( Image $image, Editor $editor ) {
+		$this->image  = $image;
+		$this->editor = $editor;
+	}
 
-    public function run() {
-        if ( ! file_exists( $this->setImage() ) ) {
-            if ( $this->editor->resizeImage( $this->aspect['width'], $this->aspect['height'] ) ) {
-                $image = $this->editor->saveImage( $this->file );
-            } else {
-                throw new \Exception( 'Could not resize image.' );
-            }
-        } else {
-            $image['path'] = $this->file;
-        }
+	public function run() {
+		if ( ! file_exists( $this->setImage() ) ) {
+			if ( $this->editor->resizeImage( $this->aspect['width'], $this->aspect['height'] ) ) {
+				$image = $this->editor->saveImage( $this->file );
+			} else {
+				throw new Exception( sprintf( 'Could not resize image: %s. Destination was: %s.', $this->image->original, $this->file ) );
+			}
+		} else {
+			$image['path'] = $this->file;
+		}
 
-        return $image;
-    }
+		return $image;
+	}
 
-    private function parseRequestedImageSize() {
-        $origWidth  = $this->editor->getWidth();
-        $origHeight = $this->editor->getHeight();
+	private function setImage() {
+		$size     = $this->parseRequestedImageSize();
+		$pathinfo = pathinfo( $this->image->original );
+		$suffix   = apply_filters( 'resizefly_resized_path', '' );
+		$dir      = $pathinfo['dirname'];
+		if ( ! empty( $suffix ) ) {
+			$dir = str_replace( $this->image->uploadDir['basedir'], trailingslashit( $this->image->uploadDir['basedir'] ) . trim( $suffix, DIRECTORY_SEPARATOR ), $pathinfo['dirname'] );
+		}
 
-        // if width or height is larger than the image itself, set it to the original width/height
-        // TODO if only one is larger, the output will be rather unexpected; maybe change to original aspect ratio
-        $width  = $this->image->resize['width'] > $origWidth ? $origWidth : $this->image->resize['width'];
-        $height = $this->image->resize['height'] > $origHeight ? $origHeight : $this->image->resize['height'];
+		return $this->file = sprintf( '%s/%s-%dx%d.%s', untrailingslashit( $dir ), $pathinfo['filename'], $size['width'], $size['height'], $pathinfo['extension'] );
+	}
 
-        if ( $width == 0 && $height == 0 ) {
-            $width  = $origWidth;
-            $height = $origHeight;
-        } elseif ( $width == 0 ) {
-            $width = round( $height * $this->editor->getRatio( 'width' ) );
-        } elseif ( $height == 0 ) {
-            $height = round( $width * $this->editor->getRatio( 'height' ) );
-        }
+	private function parseRequestedImageSize() {
+		$origWidth  = $this->editor->getWidth();
+		$origHeight = $this->editor->getHeight();
 
-        return $this->aspect = [ 'width' => (int) $width, 'height' => (int) $height ];
-    }
+		// if width or height is larger than the image itself, set it to the original width/height
+		// TODO if only one is larger, the output will be rather unexpected; maybe change to original aspect ratio
+		$width  = $this->image->resize['width'] > $origWidth ? $origWidth : $this->image->resize['width'];
+		$height = $this->image->resize['height'] > $origHeight ? $origHeight : $this->image->resize['height'];
 
-    private function setImage() {
-        $size     = $this->parseRequestedImageSize();
-        $pathinfo = pathinfo( $this->image->original );
+		if ( $width == 0 && $height == 0 ) {
+			$width  = $origWidth;
+			$height = $origHeight;
+		} elseif ( $width == 0 ) {
+			$width = round( $height * $this->editor->getRatio( 'width' ) );
+		} elseif ( $height == 0 ) {
+			$height = round( $width * $this->editor->getRatio( 'height' ) );
+		}
 
-        return $this->file = sprintf( '%s/%s-%dx%d.%s', $pathinfo['dirname'], $pathinfo['filename'], $size['width'], $size['height'], $pathinfo['extension'] );
-    }
+		return $this->aspect = [ 'width' => (int) $width, 'height' => (int) $height ];
+	}
 
 }
