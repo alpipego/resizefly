688455f555ee2e7de4fb71664edcc7c14a414d87
alpipego <alpipego@gmail.com>
2016-08-03 15:03:03 +0200

    adapt WP code style

--- src/Upload/Fake.php
+++ src/Upload/Fake.php
@@ -11,41 +11,37 @@ namespace Alpipego\Resizefly\Upload;
 
 class Fake {
 
-    private $sizes;
+	private $sizes;
 
-    public function __construct() {
+	public function run() {
+		\add_filter( 'intermediate_image_sizes_advanced', [ $this, 'getRegisteredImageSizes' ] );
+		\add_filter( 'wp_generate_attachment_metadata', [ $this, 'fakeImageResize' ] );
+	}
 
-    }
+	public function getRegisteredImageSizes( $sizes ) {
+		$this->sizes = $sizes;
 
-    public function run() {
-        \add_filter( 'intermediate_image_sizes_advanced', [ $this, 'getRegisteredImageSizes' ] );
-        \add_filter( 'wp_generate_attachment_metadata', [ $this, 'fakeImageResize' ] );
-    }
+		return [ ];
+	}
 
-    public function getRegisteredImageSizes( $sizes ) {
-        $this->sizes = $sizes;
+	public function fakeImageResize( $metadata ) {
+		foreach ( $this->sizes as $name => $size ) {
+			// figure out what size WP would make this:
+			$newsize = \image_resize_dimensions( $metadata['width'], $metadata['height'], $size['width'], $size['height'], $size['crop'] );
 
-        return [ ];
-    }
+			if ( $newsize ) {
+				$uploads = \wp_upload_dir( null, false );
+				$file    = pathinfo( realpath( $uploads['basedir'] . DIRECTORY_SEPARATOR . $metadata['file'] ) );
 
-    public function fakeImageResize( $metadata ) {
-        foreach ( $this->sizes as $name => $size ) {
-            // figure out what size WP would make this:
-            $newsize = \image_resize_dimensions( $metadata['width'], $metadata['height'], $size['width'], $size['height'], $size['crop'] );
+				// build the fake meta entry for the size in question
+				$metadata['sizes'][ $name ] = [
+					'file'   => sprintf( '%s-%sx%s.%s', $file['filename'], $newsize[4], $newsize[5], $file['extension'] ),
+					'width'  => $newsize[4],
+					'height' => $newsize[5],
+				];
+			}
+		}
 
-            if ( $newsize ) {
-                $uploads = \wp_upload_dir( null, false );
-                $file    = pathinfo( realpath( $uploads['basedir'] . DIRECTORY_SEPARATOR . $metadata['file'] ) );
-
-                // build the fake meta entry for the size in question
-                $metadata['sizes'][ $name ] = [
-                    'file'   => sprintf( '%s-%sx%s.%s', $file['filename'], $newsize[4], $newsize[5], $file['extension'] ),
-                    'width'  => $newsize[4],
-                    'height' => $newsize[5],
-                ];
-            }
-        }
-
-        return $metadata;
-    }
+		return $metadata;
+	}
 }
