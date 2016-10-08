bc0aecd19294d759d2242b40b307b2200f0be755
alpipego <alpipego@gmail.com>
2016-10-08 12:56:54 +0200

    extend imagick image editor

---
+++ src/Image/EditorImagick.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alpipego
+ * Date: 26/09/16
+ * Time: 15:54
+ */
+
+namespace Alpipego\Resizefly\Image;
+
+use WP_Image_Editor_Imagick;
+
+class EditorImagick extends WP_Image_Editor_Imagick {
+	public function getImagick() {
+		return $this->image;
+	}
+
+	public function getFile() {
+		return $this->file;
+	}
+
+}
