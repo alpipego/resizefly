6b4d7b78e84d84347c1422e150ad12e7969c6a9f
alpipego <alpipego@gmail.com>
2016-10-09 10:09:57 +0200

    add admin to own config file

--- resizefly.php
+++ resizefly.php
@@ -18,16 +18,11 @@ use Alpipego\Resizefly\Image\Handler as ImageHandler;
 use Alpipego\Resizefly\Image\Image;
 use Alpipego\Resizefly\Image\Stream;
 use Alpipego\Resizefly\Upload\Dir;
-use Alpipego\Resizefly\Upload\Fake;
 use Alpipego\Resizefly\Plugin;
 use Alpipego\Resizefly\Autoload;
 
-use Alpipego\Resizefly\Admin\OptionsPage;
-use Alpipego\Resizefly\Admin\BasicOptionsSection;
-use Alpipego\Resizefly\Admin\PathField;
-use Alpipego\Resizefly\Admin\Admin;
 
-if ( ! $check->errors() ) :
+if ( ! $check->errors() ) {
 	require_once __DIR__ . '/src/Autoload.php';
 	new Autoload();
 
@@ -41,6 +36,11 @@ if ( ! $check->errors() ) :
 		$plugin['basename'] = plugin_basename( __FILE__ );
 		$plugin['version']  = '1.3.3';
 
+		// filterable dir values
+		$plugin['cache_suffix']     = get_option( 'resizefly_resized_path', 'resizefly' );
+		$plugin['duplicate_suffix'] = apply_filters( 'resizefly_duplicate_dir', 'resizefly-duplicate' );
+
+		// filter for addons to register themselves
 		$plugin['addons'] = apply_filters( 'resizefly_addons', [] );
 
 		foreach ( $plugin['addons'] as $addonName => $addon ) {
@@ -49,42 +49,34 @@ if ( ! $check->errors() ) :
 			} );
 		}
 
-		$plugin['fake'] = function () {
-			return new Fake();
-		};
-
 		// wordpress uploads array
 		$plugin['uploads'] = function () {
 			return new Dir( wp_upload_dir( null, false ) );
 		};
 
 		$plugin->extend( 'uploads', function ( $uploads ) {
+			/** @var Dir $uploads */
 			$uploads->setUploads( wp_upload_dir( null, false ) );
 
 			return $uploads;
 		} );
 
 		// set the cache path throughout the plugin
-		$suffix               = apply_filters( 'resizefly_resized_path', get_option( 'resizefly_resized_path', '' ) );
-		$plugin['cache_path'] = trailingslashit( $plugin['uploads']->uploads['basedir'] ) . trim( $suffix, DIRECTORY_SEPARATOR );
-		$plugin['cache_url']  = trailingslashit( $plugin['uploads']->uploads['baseurl'] ) . trim( $suffix, DIRECTORY_SEPARATOR );
+		$plugin['cache_path'] = trailingslashit( $plugin['uploads']->getUploads()['basedir'] ) . trim( $plugin['cache_suffix'], DIRECTORY_SEPARATOR );
+		$plugin['cache_url']  = trailingslashit( $plugin['uploads']->getUploads()['baseurl'] ) . trim( $plugin['cache_suffix'], DIRECTORY_SEPARATOR );
+
+		// set the duplicates path
+		$plugin['duplicates_path'] = trailingslashit( $plugin['uploads']->getUploads()['basedir'] ) . trim( $plugin['duplicate_suffix'], DIRECTORY_SEPARATOR );
+
+		// Add own implementation to image editors
+		add_filter( 'wp_image_editors', function ( $editors ) {
+			array_unshift( $editors, '\\Alpipego\\Resizefly\\Image\\EditorImagick' );
+
+			return $editors;
+		} );
 
 		if ( is_admin() ) {
-			$plugin['options_page'] = function ( $plugin ) {
-				return new OptionsPage( $plugin['path'] );
-			};
-
-			$plugin['options_basic_settings'] = function ( $plugin ) {
-				return new BasicOptionsSection( $plugin['options_page']->page, $plugin['path'] );
-			};
-
-			$plugin['options_field_path'] = function ( $plugin ) {
-				return new PathField( $plugin['options_page']->page, $plugin['options_basic_settings']->optionsGroup['id'], $plugin['path'] );
-			};
-
-			$plugin['admin'] = function ( $plugin ) {
-				return new Admin( $plugin );
-			};
+			require_once __DIR__ . '/config/admin.php';
 		}
 
 		$plugin->run();
@@ -95,26 +87,21 @@ if ( ! $check->errors() ) :
 			}
 
 			if ( preg_match( '/(.*?)-([0-9]+)x([0-9]+)\.(jpe?g|png|gif)/i', urldecode( $_SERVER['REQUEST_URI'] ), $matches ) ) {
-//				$plugin['requested_file'] = array_map( function ( $val ) {
-//					while ( strpos( $val, '/./' ) ) {
-//						$val = preg_replace( '%(?:/\.{1}/)%', '/', $val );
-//					}
-//					while ( strpos( $val, '/../' ) ) {
-//						$val = preg_replace( '%(?:([^/]+?)/\.{2}/)%', '', $val );
-//					}
-//
-//					return $val;
-//				}, $matches );
+
 				$plugin['requested_file'] = $matches;
 
 				// get the correct path ("regardless" of WordPress installation path etc)
 				$plugin['image'] = function ( $plugin ) {
-					return new Image( $plugin['requested_file'], $plugin['uploads']->uploads, get_bloginfo( 'url' ), $plugin['cache_path'] );
+					return new Image( $plugin['requested_file'], $plugin['uploads'], get_bloginfo( 'url' ), $plugin['cache_path'], $plugin['duplicates_path'] );
 				};
 
+				if ( ! file_exists( $plugin['image']->getDuplicate() ) ) {
+					$plugin['duplicate_original']->rebuild( $plugin['image']->getOriginal() );
+				}
+
 				// get wp image editor and handle errors
-				$plugin['wp_image_editor'] = wp_get_image_editor( $plugin['image']->original );
-				if ( ! file_exists( $plugin['image']->original ) || is_wp_error( $plugin['wp_image_editor'] ) ) {
+				$plugin['wp_image_editor'] = wp_get_image_editor( $plugin['image']->getDuplicate() );
+				if ( ! file_exists( $plugin['image']->getOriginal() ) || is_wp_error( $plugin['wp_image_editor'] ) ) {
 					status_header( '404' );
 					@include_once get_404_template();
 
@@ -128,17 +115,16 @@ if ( ! $check->errors() ) :
 
 				// create image handling instance
 				$plugin['image_handler'] = function ( $plugin ) {
-					return new ImageHandler( $plugin['image'], $plugin['image_editor'], $plugin['cache_path'] );
+					return new ImageHandler( $plugin['image'], $plugin['image_editor'], $plugin['cache_path'], $plugin['duplicates_path'] );
 				};
 
 				// output stream the resized image
 				$plugin['output'] = function ( $plugin ) {
-					return new Stream( wp_get_image_editor( $plugin['image_handler']->file ) );
+					return new Stream( wp_get_image_editor( $plugin['image_handler']->getImage() ) );
 				};
 
 				$plugin->run();
 			}
 		} );
 	} );
-
-endif;
+}
