f91e0b1cb0e114034445e97fc12b13cc3e0c0fe2
alpipego <alpipego@gmail.com>
2017-02-03 13:32:31 +0000

    add density to media lib

--- src/Upload/Dir.php
+++ src/Upload/Dir.php
@@ -14,128 +14,142 @@ namespace Alpipego\Resizefly\Upload;
  * @package Alpipego\Resizefly\Upload
  */
 class Dir {
-    /**
-     * @var
-     */
-    private $uploads;
+	/**
+	 * @var
+	 */
+	private $uploads;
 
-    /**
-     * Dir constructor.
-     */
-    public function __construct() {
-        \add_filter( 'upload_dir', [ $this, 'resolvePath' ] );
-        \add_filter( 'upload_dir', [ $this, 'resolveUrl' ] );
-    }
+	/**
+	 * Dir constructor.
+	 */
+	public function __construct() {
+		\add_filter( 'upload_dir', [ $this, 'resolvePath' ] );
+		\add_filter( 'upload_dir', [ $this, 'resolveUrl' ] );
+	}
 
-    public function run() {
-        \add_filter( 'resizefly_filter_url', [ $this, 'filterImageUrl' ] );
-        \add_filter( 'wp_prepare_attachment_for_js', function ( $post ) {
-            foreach ( $post['sizes'] as $key => $size ) {
-                $post['sizes'][ $key ]['url'] = $this->filterImageUrl( $size['url'] );
-            }
+	public function run() {
+		\add_filter( 'resizefly_filter_url', [ $this, 'filterImageUrl' ] );
+		\add_filter( 'wp_prepare_attachment_for_js', function ( $post ) {
+			if ( isset( $post['sizes'] ) ) {
+				foreach ( $post['sizes'] as $key => $size ) {
+					$post['sizes'][ $key ]['url'] = $this->filterImageUrl( $size['url'] );
+					$post['sizes'][ $key ]['url'] = $this->addDensity( $post['sizes'][ $key ]['url'], 1 );
+				}
+			}
 
-            return $post;
-        } );
+			return $post;
+		} );
 
-        \add_filter( 'wp_get_attachment_image_src', function ( $image ) {
-            $image[0] = $this->filterImageUrl( $image[0] );
+		\add_filter( 'wp_get_attachment_image_src', function ( $image ) {
+			$image[0] = $this->filterImageUrl( $image[0] );
 
-            return $image;
-        } );
-    }
+			return $image;
+		} );
+	}
 
-    public function filterImageUrl( $url ) {
-        if ( ! preg_match( '%\d+?x\d+?@?\d?\.(png|jpe?g|gif)%', $url ) ) {
-            return $url;
-        }
-        $resizeUrl = \trailingslashit( $this->uploads['baseurl'] ) . trim( get_option( 'resizefly_resized_path', 'resizefly' ), DIRECTORY_SEPARATOR );
-        if ( strpos( $url, $resizeUrl ) === false ) {
-            $url = str_replace( $this->uploads['baseurl'], $resizeUrl, $url );
-        }
+	public function filterImageUrl( $url ) {
+		if ( ! preg_match( '%\d+?x\d+?@?\d?\.(png|jpe?g|gif)%', $url ) ) {
+			return $url;
+		}
+		$resizeUrl = \trailingslashit( $this->uploads['baseurl'] ) . trim( get_option( 'resizefly_resized_path', 'resizefly' ), DIRECTORY_SEPARATOR );
+		if ( strpos( $url, $resizeUrl ) === false ) {
+			$url = str_replace( $this->uploads['baseurl'], $resizeUrl, $url );
+		}
 
-        return $url;
-    }
+		return $url;
+	}
 
-    /**
-     * @return array wp_uploads_dir()
-     */
-    public function getUploads() {
-        return $this->uploads;
-    }
+	private function addDensity( $url, $density ) {
+		if ( ! preg_match( '%(.*?)(\d+x\d+)(@\d)?\.(png|jpe?g|gif)%', $url, $matches ) ) {
+			return $url;
+		}
 
-    /**
-     * Wrap `wp_uploads_dir`
-     *
-     * @param $uploads array wp_uploads_dir
-     *
-     * @return array wp_uploads_dir
-     */
-    public function setUploads( $uploads ) {
-        return $this->uploads = $uploads;
-    }
+		if ( empty( $matches[3] ) ) {
+			$url = sprintf( '%s%s@%d.%s', $matches[1], $matches[2], $density, $matches[4] );
+		}
 
-    /**
-     * Resolve all relative path parts
-     *
-     * @param $uploads array wp_uploads_dir
-     *
-     * @return array wp_uploads_dir
-     */
-    public function resolvePath( $uploads ) {
-        $uploads['path']    = $this->normalizePath( $uploads['path'] );
-        $uploads['basedir'] = $this->normalizePath( $uploads['basedir'] );
+		return $url;
+	}
 
-        return $uploads;
-    }
+	/**
+	 * @return array wp_uploads_dir()
+	 */
+	public function getUploads() {
+		return $this->uploads;
+	}
 
-    /**
-     * @param $path
-     *
-     * @return string
-     */
-    private function normalizePath( $path ) {
-        return array_reduce( explode( '/', $path ), function ( $a, $b ) {
-            if ( $a === 0 ) {
-                $a = '/';
-            }
+	/**
+	 * Wrap `wp_uploads_dir`
+	 *
+	 * @param $uploads array wp_uploads_dir
+	 *
+	 * @return array wp_uploads_dir
+	 */
+	public function setUploads( $uploads ) {
+		return $this->uploads = $uploads;
+	}
 
-            if ( $b === '' || $b === '.' ) {
-                return $a;
-            }
+	/**
+	 * Resolve all relative path parts
+	 *
+	 * @param $uploads array wp_uploads_dir
+	 *
+	 * @return array wp_uploads_dir
+	 */
+	public function resolvePath( $uploads ) {
+		$uploads['path']    = $this->normalizePath( $uploads['path'] );
+		$uploads['basedir'] = $this->normalizePath( $uploads['basedir'] );
 
-            if ( $b === '..' ) {
-                return dirname( $a );
-            }
+		return $uploads;
+	}
 
-            return preg_replace( '/\/+/', '/', "$a/$b" );
-        }, 0 );
-    }
+	/**
+	 * @param $path
+	 *
+	 * @return string
+	 */
+	private function normalizePath( $path ) {
+		return array_reduce( explode( '/', $path ), function ( $a, $b ) {
+			if ( $a === 0 ) {
+				$a = '/';
+			}
 
-    /**
-     * Resolve all relative url parts
-     *
-     * @param $uploads array wp_uploads_dir
-     *
-     * @return array wp_uploads_dir
-     */
-    public function resolveUrl( $uploads ) {
-        // resolve all /./
-        while ( strpos( $uploads['url'], '/./' ) ) {
-            $uploads['url'] = preg_replace( '%(?:/\.{1}/)%', '/', $uploads['url'] );
-        }
-        while ( strpos( $uploads['baseurl'], '/./' ) ) {
-            $uploads['baseurl'] = preg_replace( '%(?:/\.{1}/)%', '/', $uploads['baseurl'] );
-        }
+			if ( $b === '' || $b === '.' ) {
+				return $a;
+			}
 
-        // resolve all /../
-        while ( strpos( $uploads['url'], '/../' ) ) {
-            $uploads['url'] = preg_replace( '%(?:([^/]+?)/\.{2}/)%', '', $uploads['url'] );
-        }
-        while ( strpos( $uploads['baseurl'], '/../' ) ) {
-            $uploads['baseurl'] = preg_replace( '%(?:([^/]+?)/\.{2}/)%', '', $uploads['baseurl'] );
-        }
+			if ( $b === '..' ) {
+				return dirname( $a );
+			}
 
-        return $uploads;
-    }
+			return preg_replace( '/\/+/', '/', "$a/$b" );
+		}, 0 );
+	}
+
+	/**
+	 * Resolve all relative url parts
+	 *
+	 * @param $uploads array wp_uploads_dir
+	 *
+	 * @return array wp_uploads_dir
+	 */
+	public function resolveUrl( $uploads ) {
+		// resolve all /./
+		while ( strpos( $uploads['url'], '/./' ) ) {
+			$uploads['url'] = preg_replace( '%(?:/\.{1}/)%', '/', $uploads['url'] );
+		}
+		while ( strpos( $uploads['baseurl'], '/./' ) ) {
+			$uploads['baseurl'] = preg_replace( '%(?:/\.{1}/)%', '/', $uploads['baseurl'] );
+		}
+
+		// resolve all /../
+		while ( strpos( $uploads['url'], '/../' ) ) {
+			$uploads['url'] = preg_replace( '%(?:([^/]+?)/\.{2}/)%', '', $uploads['url'] );
+		}
+		while ( strpos( $uploads['baseurl'], '/../' ) ) {
+			$uploads['baseurl'] = preg_replace( '%(?:([^/]+?)/\.{2}/)%', '', $uploads['baseurl'] );
+		}
+
+		return $uploads;
+	}
 }
- 
