3723d89b466c1e3fd5ebfab51d2811503fa25a6b
alpipego <alpipego@gmail.com>
2017-07-18 11:49:07 +0200

    change editor wrapper name, add interface

--- app/config/image.php
+++ app/config/image.php
@@ -6,14 +6,13 @@
  * Time: 15:29
  */
 
-use Alpipego\Resizefly\Image\Editor;
+use Alpipego\Resizefly\Image\EditorWrapper;
 use Alpipego\Resizefly\Image\Handler;
-use function Alpipego\Resizefly\object;
 
 return [
-    Editor::class  => object()
+    EditorWrapper::class => Alpipego\Resizefly\object()
         ->constructorParam('editor', 'wp_image_editor'),
-    Handler::class => object()
+    Handler::class       => Alpipego\Resizefly\object()
         ->constructorParam('cachePath', 'options.cache.path')
         ->constructorParam('duplicatesPath', 'options.duplicates.path'),
 ];
---
+++ src/Image/EditorWrapper.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alpipego
+ * Date: 05/20/16
+ * Time: 12:07 PM
+ */
+
+namespace Alpipego\Resizefly\Image;
+
+use WP_Image_Editor;
+
+/**
+ * Wrapper for `WP_Image_Editor`
+ * @package Alpipego\Resizefly\Image
+ */
+final class EditorWrapper implements EditorWrapperInterface
+{
+    /**
+     * @var WP_Image_Editor
+     */
+    private $editor;
+
+    /**
+     * inject image editor
+     *
+     * @param WP_Image_Editor $editor
+     */
+    public function __construct(WP_Image_Editor $editor)
+    {
+        $this->editor = $editor;
+    }
+
+    /**
+     * Get the image ratio
+     *
+     * @param array $aspect if width or height should be calculated; accepts 'width', 'w', 'height', 'h'
+     *
+     * @return float|int
+     */
+    public function getRatio(array $aspect)
+    {
+        if (in_array($aspect, ['width', 'w'])) {
+            return $this->getWidth() / $this->getHeight();
+        }
+
+        if (in_array($aspect, ['height', 'h'])) {
+            return $this->getHeight() / $this->getWidth();
+        }
+
+        return 1;
+    }
+
+    public function getWidth()
+    {
+        return (int) $this->editor->get_size()['width'];
+    }
+
+    public function getHeight()
+    {
+        return (int) $this->editor->get_size()['height'];
+    }
+
+    public function resizeImage($width, $height, $focalX, $focalY)
+    {
+        $origWidth  = $this->getWidth();
+        $origHeight = $this->getHeight();
+        $ratio      = max($width / $origWidth, $height / $origHeight);
+        $srcX       = round(($origWidth - $width / $ratio) * $focalX / 100);
+        $srcY       = round(($origHeight - $height / $ratio) * $focalY / 100);
+
+        return $this->editor->crop($srcX, $srcY, $width / $ratio, $height / $ratio, $width, $height);
+    }
+
+    public function saveImage($file)
+    {
+        do_action('resizefly_before_save', $file, $this->editor);
+
+        return $this->editor->save($file);
+    }
+
+    /**
+     * Stream the image
+     *
+     * @return void
+     */
+    public function streamImage()
+    {
+        $cacheAge = apply_filters('resizefly_cache_age', 31536000);
+        http_response_code(200);
+        header('HTTP/1.1 200 OK');
+        header('Pragma: public');
+        header('Cache-Control: max-age=' . $cacheAge . ', public');
+        header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + $cacheAge));
+
+        if (method_exists($this->editor, 'getImagick')) {
+            header('Content-Length: ' . strlen($this->editor->getImagick()->getImageBlob()));
+        }
+        $this->editor->stream();
+        exit;
+    }
+
+    public function getQuality()
+    {
+        return (int) $this->editor->get_quality();
+    }
+}
