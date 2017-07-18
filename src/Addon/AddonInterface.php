657aa5dc106c63a5aa8cc78aae1e3a3222eb1077
alpipego <alpipego@gmail.com>
2017-07-18 11:48:35 +0200

    add addon interface

---
+++ src/Addon/AddonInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alpipego
+ * Date: 18.07.2017
+ * Time: 11:34
+ */
+
+namespace Alpipego\Resizefly\Addon;
+
+
+interface AddonInterface
+{
+    public function run();
+}
