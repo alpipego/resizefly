950395cefbaf9b97216f1a8e17d01690912c4660
alpipego <alpipego@gmail.com>
2017-07-20 10:23:59 +0200

    add longer variable name phpmd

--- src/Autoload.php
+++ src/Autoload.php
@@ -18,19 +18,21 @@ class Autoload {
 	 */
 	private $dir;
 	/**
-	 * @var string $ns namespace to load
+	 * @var string $namespace namespace to load
 	 */
-	private $ns;
+	private $namespace;
 
-	/**
-	 * Autoload constructor.
-	 *
-	 * @param string $dir pass dir, default to current dir
-	 * @param string $ns pass namespace, default to current namespace
-	 */
-	public function __construct( $dir = __DIR__, $ns = __NAMESPACE__ ) {
-        $this->dir = $dir;
-        $this->ns  = $ns;
+    /**
+     * Autoload constructor.
+     *
+     * @param string $dir pass dir, default to current dir
+     * @param string $namespace
+     *
+     * @internal param string $ns pass namespace, default to current namespace
+     */
+	public function __construct( $dir = __DIR__, $namespace = __NAMESPACE__ ) {
+        $this->dir       = $dir;
+        $this->namespace = $namespace;
         $this->load();
     }
 
@@ -38,14 +40,14 @@ class Autoload {
 	 * load classes
 	 */
 	public function load() {
-        \spl_autoload_register( function ( $class ) {
+        spl_autoload_register( function ( $class ) {
             $class = ltrim( $class, '\\' );
 
-            if ( strpos( $class, $this->ns ) !== 0 ) {
+            if (strpos( $class, $this->namespace ) !== 0 ) {
                 return;
             }
 
-            $class = str_replace( $this->ns, '', $class );
+            $class = str_replace( $this->namespace, '', $class );
             $path  = $this->dir . str_replace( '\\', DIRECTORY_SEPARATOR, $class ) . '.php';
             require_once $path;
         } );
