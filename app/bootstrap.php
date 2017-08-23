76b2846d7bfd7b7a5deeb51061de1d8fb7e586fc
alpipego <alpipego@gmail.com>
2017-08-23 09:25:48 +0200

    replace autoloading with composers classloader

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -6,7 +6,10 @@
  * Time: 10:45
  */
 
-use Alpipego\Resizefly\Autoload;
+use Alpipego\Resizefly\Common\Composer\Autoload\ClassLoader;
 
-require_once __DIR__ . '/../src/Autoload.php';
-new Autoload();
+require_once __DIR__ . '/../src/Common/Composer/Autoload/ClassLoader.php';
+
+$loader = new ClassLoader();
+$loader->setPsr4('Alpipego\\Resizefly\\', realpath(__DIR__ . '/../src/'));
+$loader->register();
