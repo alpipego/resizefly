4ff8294f990f2cc78822118dcf6371dfde7b4eb9
alpipego <alpipego@gmail.com>
2017-08-23 09:25:02 +0200

    remove use statement for function to keep php5.5 compat

--- app/config/plugin.php
+++ app/config/plugin.php
@@ -11,18 +11,18 @@ use Alpipego\Resizefly\Image\ImageInterface;
 use Alpipego\Resizefly\Upload\DuplicateOriginal;
 use Alpipego\Resizefly\Upload\Filter;
 use Alpipego\Resizefly\Upload\Uploads;
-use function Alpipego\Resizefly\object;
 use Alpipego\Resizefly\Upload\UploadsInterface;
 
 return [
     UploadsInterface::class  => Uploads::class,
     ImageInterface::class    => Image::class,
     'cacheUrl'               => 'options.cache.url',
-    Filter::class            => object(),
-    Image::class             => object()
+    Filter::class            => Alpipego\Resizefly\object()
+        ->constructorParam('imgRegex', 'config.imgregex'),
+    Image::class             => Alpipego\Resizefly\object()
         ->constructorParam('siteUrl', 'config.siteurl')
         ->constructorParam('cachePath', 'options.cache.path')
         ->constructorParam('duplicatesPath', 'options.duplicates.path'),
-    DuplicateOriginal::class => object()
+    DuplicateOriginal::class => Alpipego\Resizefly\object()
         ->constructorParam('duplicateDir', 'options.duplicate.suffix'),
 ];
