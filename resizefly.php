9d5c6af0a61962963a9397a4e5c55449be153315
alpipego <alpipego@gmail.com>
2017-08-23 10:16:43 +0200

    use composer classloader, put img regex in config, reformat code

--- resizefly.php
+++ resizefly.php
@@ -19,11 +19,11 @@ $check = new Resizefly_Version_Check(__FILE__);
 use Alpipego\Resizefly\Plugin;
 use Alpipego\Resizefly\Upload\Uploads;
 
-if ( ! $check->errors()) {
-    require_once __DIR__ . '/app/bootstrap.php';
+if (! $check->errors()) {
+    $classLoader = require_once __DIR__ . '/app/bootstrap.php';
     require_once __DIR__ . '/app/functions.php';
 
-    add_action('plugins_loaded', function () {
+    add_action('plugins_loaded', function () use ($classLoader) {
         $plugin = new Plugin();
         $plugin->addDefiniton(__DIR__ . '/app/config/plugin.php');
         $plugin->loadTextdomain(__DIR__ . '/languages');
@@ -31,29 +31,38 @@ if ( ! $check->errors()) {
         $plugin['config.path']     = trailingslashit(realpath(plugin_dir_path(__FILE__)));
         $plugin['config.url']      = plugin_dir_url(__FILE__);
         $plugin['config.basename'] = plugin_basename(__FILE__);
-        $plugin['config.siteurl'] = get_bloginfo('url');
+        $plugin['config.siteurl']  = get_bloginfo('url');
         $plugin['config.version']  = '1.3.5';
+        $plugin['config.imgregex']    = apply_filters(
+            'resizefly/config/imgregex',
+            '/(?<file>.*?)-(?<width>[0-9]+)x(?<height>[0-9]+)@?(?<density>[0-3])?\.(?<ext>jpe?g|png|gif)/i'
+        );
         // settings/filterable configuration values
         $plugin['options.cache.suffix']     = get_option('resizefly_resized_path', 'resizefly');
         $plugin['options.duplicate.suffix'] = apply_filters('resizefly_duplicate_dir', 'resizefly-duplicate');
 
         // set the cache path throughout the plugin
         $plugin['options.cache.path'] = function (Plugin $plugin) {
-            return trailingslashit($plugin->get(Uploads::class)->getBasePath()) . trim($plugin->get('options.cache.suffix'),
+            return trailingslashit($plugin->get(Uploads::class)
+                                          ->getBasePath()) . trim($plugin->get('options.cache.suffix'),
                     DIRECTORY_SEPARATOR);
         };
         $plugin['options.cache.url']  = function (Plugin $plugin) {
-            return trailingslashit($plugin->get(Uploads::class)->getBaseUrl()) . trim($plugin->get('options.cache.suffix'),
+            return trailingslashit($plugin->get(Uploads::class)
+                                          ->getBaseUrl()) . trim($plugin->get('options.cache.suffix'),
                     DIRECTORY_SEPARATOR);
         };
         // set the duplicates path
         $plugin['options.duplicates.path'] = function (Plugin $plugin) {
-            return trailingslashit($plugin->get(Uploads::class)->getBasePath()) . trim($plugin->get('options.duplicate.suffix'),
+            return trailingslashit($plugin->get(Uploads::class)
+                                          ->getBasePath()) . trim($plugin->get('options.duplicate.suffix'),
                     DIRECTORY_SEPARATOR);
         };
 
+        $plugin->offsetSet('loader', $classLoader);
+
         // filter for addons to register themselves
-        $plugin['addons'] = apply_filters('resizefly_addons', []);
+        $plugin->offsetSet('addons', apply_filters('resizefly_addons', []));
 
         foreach ($plugin->get('addons') as $addonName => $addon) {
             add_filter("resizefly_plugin_{$addonName}", function () use ($plugin) {
