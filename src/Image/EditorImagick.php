89f4fa1d7f91a10a35c787f8ec5cbd52011c8435
alpipego <alpipego@gmail.com>
2017-08-24 12:41:16 +0200

    improve imagick wrapper

--- src/Image/EditorImagick.php
+++ src/Image/EditorImagick.php
@@ -10,20 +10,77 @@ namespace Alpipego\Resizefly\Image;
 
 use WP_Image_Editor_Imagick;
 
-class EditorImagick extends WP_Image_Editor_Imagick {
-	public function getImagick() {
-		return $this->image;
-	}
+class EditorImagick extends WP_Image_Editor_Imagick
+{
+    public function setOption($key, $value)
+    {
+        return $this->image->setOption($key, $value);
+    }
 
-	public function getFile() {
-		return $this->file;
-	}
+    public function setColorspace($colorspace)
+    {
+        return $this->image->setColorspace($colorspace);
+    }
 
-	public function blurImage($radius, $float) {
-		return $this->image->blurImage($radius, $float);
-	}
+    public function posterizeImage($levels, $dither)
+    {
+        return $this->image->posterizeImage($levels, $dither);
+    }
 
-	public function getImageBlob() {
-		return $this->image->getImageBlob();
-	}
+    public function setResourceLimit($type, $limit)
+    {
+        return $this->image->setResourceLimit($type, $limit);
+    }
+
+    public function getResourceLimit($type)
+    {
+        return $this->image->getResourceLimit($type);
+    }
+
+    public function stripImage()
+    {
+        return $this->image->stripImage();
+    }
+
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    public function blurImage($radius, $float)
+    {
+        return $this->image->blurImage($radius, $float);
+    }
+
+    public function getImageBlob()
+    {
+        return $this->image->getImageBlob();
+    }
+
+    public function setImageFormat($format)
+    {
+        return $this->image->setImageFormat($format);
+    }
+
+    public function set_quality($quality = null)
+    {
+        // call grandparent set_quality
+        $quality_result = \WP_Image_Editor::set_quality($quality);
+        if (is_wp_error($quality_result)) {
+            return $quality_result;
+        }
+        $quality = $this->get_quality();
+
+        try {
+            $this->image->setImageCompressionQuality($quality);
+            $this->image->setInterlaceScheme(\Imagick::INTERLACE_PLANE);
+            if ($this->mime_type === 'image/jpeg') {
+                $this->image->setImageCompression(\Imagick::COMPRESSION_JPEG);
+            }
+        } catch (\Exception $e) {
+            return new \WP_Error('image_quality_error', $e->getMessage());
+        }
+
+        return true;
+    }
 }
