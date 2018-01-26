c4e7befdd7714ea7a523ce67766d865ddc129b42
alpipego <alpipego@gmail.com>
2018-01-26 10:37:16 +0700

    add id to table, improve code

--- views/field/resizefly-sizes.php
+++ views/field/resizefly-sizes.php
@@ -6,59 +6,61 @@
  * User: alpipego
  * Date: 25.06.2017
  * Time: 12:42
+ *
+ * @var array $args passed view arguments
  */
 ?>
-<table class="widefat rzf-image-sizes">
+<table class="widefat rzf-image-sizes" id="rzf-image-sizes">
     <thead>
     <tr>
-        <th><?= __( 'Active', 'resizefly' ); ?></th>
-        <th><?= __( 'Width', 'resizefly' ); ?></th>
-        <th><?= __( 'Height', 'resizefly' ); ?></th>
-        <th><?= __( 'Crop', 'resizefly' ); ?></th>
-        <th><?= __( 'Name', 'resizefly' ); ?></th>
+        <th><?= __('Active', 'resizefly'); ?></th>
+        <th><?= __('Width', 'resizefly'); ?></th>
+        <th><?= __('Height', 'resizefly'); ?></th>
+        <th><?= __('Crop', 'resizefly'); ?></th>
+        <th><?= __('Name', 'resizefly'); ?></th>
     </tr>
     </thead>
     <tfoot>
     <tr>
-        <th><?= __( 'Active', 'resizefly' ); ?></th>
-        <th><?= __( 'Width', 'resizefly' ); ?></th>
-        <th><?= __( 'Height', 'resizefly' ); ?></th>
-        <th><?= __( 'Crop', 'resizefly' ); ?></th>
-        <th><?= __( 'Name', 'resizefly' ); ?></th>
+        <th><?= __('Active', 'resizefly'); ?></th>
+        <th><?= __('Width', 'resizefly'); ?></th>
+        <th><?= __('Height', 'resizefly'); ?></th>
+        <th><?= __('Crop', 'resizefly'); ?></th>
+        <th><?= __('Name', 'resizefly'); ?></th>
     </tr>
     </tfoot>
     <tbody>
-	<?php
-	foreach ( $args['image_sizes'] as $name => $size ) :
-		?>
+    <?php
+    foreach ($args['image_sizes'] as $name => $size) :
+        ?>
         <tr>
             <td>
-                <input type="checkbox" name="resizefly_sizes[<?= $name; ?>][active]" <?php checked( $size['active'], 1 ); ?>>
+                <input type="checkbox" name="resizefly_sizes[<?= $name; ?>][active]" <?php checked($size['active'], 1); ?>>
             </td>
             <td>
-				<?= $size['width']; ?>
+                <?= $size['width']; ?>
                 <input type="hidden" name="resizefly_sizes[<?= $name; ?>][width]" value="<?= $size['width']; ?>">
             </td>
             <td>
-				<?= $size['height']; ?>
+                <?= $size['height']; ?>
                 <input type="hidden" name="resizefly_sizes[<?= $name; ?>][height]" value="<?= $size['height']; ?>">
             </td>
             <td>
-				<?php
-				if ( $size['crop'] ) {
-					if ( is_array( $size['crop'] ) ) {
-						printf( '%s, %s', $size['crop'][0], $size['crop'][1] );
-					} else {
-						echo 'center, center';
-					}
-				}
-				?>
-                <input type="hidden" name="resizefly_sizes[<?= $name; ?>][crop]" value="<?= implode( ', ', (array) $size['crop'] ); ?>">
+                <?php
+                if ($size['crop']) {
+                    if (is_array($size['crop'])) {
+                        printf('%s, %s', $size['crop'][0], $size['crop'][1]);
+                    } else {
+                        echo 'center, center';
+                    }
+                }
+                ?>
+                <input type="hidden" name="resizefly_sizes[<?= $name; ?>][crop]" value="<?= implode(', ', (array)$size['crop']); ?>">
             </td>
             <td><?= $name; ?></td>
         </tr>
-		<?php
-	endforeach;
-	?>
+    <?php
+    endforeach;
+    ?>
     </tbody>
 </table>
