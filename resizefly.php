f0dc68e415240665ffb13f1d919008a7d1420d03
alpipego <alpipego@gmail.com>
2018-03-13 14:24:10 +0700

    Bump version to 2.0.5

--- resizefly.php
+++ resizefly.php
@@ -4,7 +4,7 @@
  * Plugin Name: Resizefly
  * Description: Dynamically resize your images on the fly
  * Plugin URI:  https://resizefly.com/
- * Version:     2.0.4
+ * Version:     2.0.5
  * Author:      alpipego
  * Author URI:  https://alpipego.com/
  * License:     MIT
