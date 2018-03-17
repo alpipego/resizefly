17739bd7e10ea9fe5e7dba3e69bc5b19ef3d6b92
alpipego <alpipego@gmail.com>
2018-03-17 12:22:06 +0700

    the previous bug of saving the file as array might not have been fully resolved

--- app/actions/wp-get-attachment-src.php
+++ app/actions/wp-get-attachment-src.php
@@ -6,10 +6,24 @@ add_filter( 'wp_get_attachment_image_src', function ( $image, $imageId, $size ) 
 	}
 
 	$imageMeta = wp_get_attachment_metadata( $imageId );
+	$update    = false;
 	if ( empty( $imageMeta ) ) {
 		return $image;
 	}
 
+	// fix a bug from all version 1 images
+	if ( empty( $imageMeta['sizes']['full'] ) ) {
+		$imageMeta['sizes']['full'] = [
+			'file'   => array_slice( explode( DIRECTORY_SEPARATOR, $imageMeta['file'] ), - 1 )[0],
+			'width'  => $imageMeta['width'],
+			'height' => $imageMeta['height'],
+		];
+		$update                     = true;
+	}
+	if ( is_array( $imageMeta['sizes']['full']['file'] ) ) {
+		$imageMeta['sizes']['full']['file'] = $imageMeta['sizes']['full']['file'][0];
+		$update                             = true;
+	}
 
 	// does our custom size even exist in the image meta data?
 	// if not, then we will have to create it
@@ -18,15 +32,6 @@ add_filter( 'wp_get_attachment_image_src', function ( $image, $imageId, $size ) 
 
 		if ( empty( $sizes[ $size ] ) ) {
 			return $image;
-		}
-
-		// fix a bug from all version 1 images
-		if ( empty( $imageMeta['sizes']['full'] ) ) {
-			$imageMeta['sizes']['full'] = [
-				'file'   => array_slice( explode( DIRECTORY_SEPARATOR, $imageMeta['file'] ), - 1 )[0],
-				'width'  => $imageMeta['width'],
-				'height' => $imageMeta['height'],
-			];
 		}
 
 		$file = pathinfo( $image[0] );
@@ -51,6 +56,10 @@ add_filter( 'wp_get_attachment_image_src', function ( $image, $imageId, $size ) 
 			$size !== 'full',
 		];
 
+		$update = true;
+	}
+
+	if ( $update ) {
 		wp_update_attachment_metadata( $imageId, $imageMeta );
 	}
 
