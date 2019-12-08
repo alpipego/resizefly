6e5abe37097959115d993edc07055e32c3847f7d
Alex Goller <alpipego@protonmail.com>
2019-12-08 11:08:45 +0100

    create / delete custom tables for queue

--- app/actions/activation.php
+++ app/actions/activation.php
@@ -5,26 +5,56 @@
  * Date: 1.11.2017
  * Time: 12:21.
  *
- * @var \Alpipego\Resizefly\Plugin $plugin
+ * @var Plugin $plugin
  */
 
-// try to move thumbnail files
-use Alpipego\Resizefly\Admin\Sizes\SizesField;
 use Alpipego\Resizefly\Common\Psr\Container\ContainerExceptionInterface;
 use Alpipego\Resizefly\Common\Psr\Container\NotFoundExceptionInterface;
+use Alpipego\Resizefly\Plugin;
 
 if (version_compare(get_option('resizefly_version'), $plugin['config.version'], '<')) {
+    // try to move thumbnail files
     try {
         $plugin->get('Alpipego\Resizefly\Upload\Cache')->warmUpAll();
     } catch (NotFoundExceptionInterface $e) {
     } catch (ContainerExceptionInterface $e) {
     }
 
+    require_once ABSPATH.'wp-admin/includes/upgrade.php';
+
+    /** @var wpdb $wpdb */
+    $wpdb = $GLOBALS['wpdb'];
+    $before = $wpdb->hide_errors();
+    $charset_collate = $wpdb->get_charset_collate();
+
+    dbDelta(<<<JOBS
+CREATE TABLE {$wpdb->prefix}rzf_queue_jobs (
+id bigint(20) NOT NULL AUTO_INCREMENT,
+job longtext NOT NULL,
+attempts tinyint(3) NOT NULL DEFAULT 0,
+reserved_at datetime DEFAULT NULL,
+available_at datetime NOT NULL,
+created_at datetime NOT NULL,
+PRIMARY KEY  (id)
+) $charset_collate;
+JOBS
+    );
+
+    dbDelta(<<<FAILS
+CREATE TABLE {$wpdb->prefix}rzf_queue_failures (
+id bigint(20) NOT NULL AUTO_INCREMENT,
+job longtext NOT NULL,
+error text DEFAULT NULL,
+failed_at datetime NOT NULL,
+PRIMARY KEY  (id)
+) $charset_collate;
+FAILS
+    );
+
+    $wpdb->show_errors($before);
+
     // update the version option
     add_option('resizefly_version_initial', $plugin['config.version']);
     update_option('resizefly_version', $plugin['config.version']);
 }
-
-/** @var SizesField $imageSizes */
-$imageSizes = $plugin->get(SizesField::class);
-$imageSizes->updateImageSizes(true);
+;
--- uninstall.php
+++ uninstall.php
@@ -5,20 +5,29 @@
  * Date: 31.10.2017
  * Time: 10:42.
  */
-if (! defined('WP_UNINSTALL_PLUGIN')) {
+if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit();
 }
 
 // simple uninstallation for now
 $options = get_option('resizefly_options');
 
-global $wp_filesystem, $wpdb;
+/** @var WP_Filesystem_Base $wp_filesystem */
+$wp_filesystem = $GLOBALS['wp_filesystem'];
+/** @var wpdb $wpdb */
+$wpdb = $GLOBALS['wpdb'];
 
 foreach ([$options['cache']['path'], $options['duplicates']['path']] as $dir) {
     if (is_dir($dir)) {
         $wp_filesystem->rmdir($dir, true);
     }
 }
+
+// remove queue databases
+$before = $wpdb->hide_errors();
+$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}rzf_queue_jobs");
+$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}rzf_queue_failures");
+$wpdb->show_errors($before);
 
 // delete database options
 $options = $wpdb->get_col("SELECT option_name FROM {$wpdb->options} WHERE option_name LIKE 'resizefly_%';");
