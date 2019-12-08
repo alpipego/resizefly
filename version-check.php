aaa7162b64572723d7a04c4ffe72e246f941c9c7
Alex Goller <alpipego@protonmail.com>
2019-12-08 12:28:04 +0100

    simplify version-check, remove plugin deactivation

--- version-check.php
+++ version-check.php
@@ -1,109 +1,89 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: alpipego
- * Date: 15/06/16
- * Time: 19:11
+ * PHP 5.2 compatible check if we can run Resizefly
  */
-class Resizefly_Version_Check {
-	const PHP_VERSION = '5.6';
-	const WP_VERSION = '4.7';
+class Resizefly_Version_Check
+{
+    const PHP_VERSION = '5.6';
+    const WP_VERSION = '4.7';
 
-	private $versions = array(
-		'php'    => true,
-		'wp'     => true,
-		'editor' => true,
+    private $versions = array(
+        'php'    => true,
+        'wp'     => true,
+        'editor' => true,
     );
-	private $file;
 
-	public function __construct( $file ) {
-		$this->file = $file;
-		$this->run();
-	}
+    public function __construct()
+    {
+        $this->checkVersions();
 
-	private function run() {
-		$this->checkVersions();
+        if ($this->errors()) {
+            add_action('admin_notices', array($this, 'notice'));
+        }
+    }
 
-		if ( $this->errors() ) {
-			if ( ! function_exists( 'deactivate_plugins' ) ) {
-				require_once ABSPATH . 'wp-admin/includes/plugin.php';
-			}
+    private function checkVersions()
+    {
+        $this->checkPHPVersion();
+        $this->checkWPVersion();
+        $this->checkImageEditor();
+    }
 
-			add_action( 'admin_notices', [ $this, 'notice' ] );
-			add_action( 'plugins_loaded', [ $this, 'deactivate' ] );
-			add_action( 'admin_init', [ $this, 'deactivate' ] );
-			add_action( 'init', [ $this, 'deactivate' ] );
-			register_activation_hook( $this->file, [ $this, 'deactivate' ] );
-		}
-	}
+    private function checkPHPVersion()
+    {
+        if (version_compare(PHP_VERSION, self::PHP_VERSION, '<')) {
+            $this->versions['php'] = false;
+        }
 
-	private function checkVersions() {
-		$this->checkPHPVersion();
-		$this->checkWPVersion();
-		$this->checkImageEditor();
-	}
+        return $this->versions;
+    }
 
-	private function checkPHPVersion() {
-		if ( version_compare( PHP_VERSION, self::PHP_VERSION, '<' ) ) {
-			$this->versions['php'] = false;
-		}
+    private function checkWPVersion()
+    {
+        if (version_compare(get_bloginfo('version'), self::WP_VERSION, '<=')) {
+            $this->versions['wp'] = false;
+        }
 
-		return $this->versions;
-	}
+        return $this->versions;
+    }
 
-	private function checkWPVersion() {
-		if ( version_compare( get_bloginfo( 'version' ), self::WP_VERSION, '<=' ) ) {
-			$this->versions['wp'] = false;
-		}
+    private function checkImageEditor()
+    {
+        if ($this->versions['wp'] && $this->versions['php']) {
+            $editor = wp_get_image_editor(__DIR__.'/rzf-test.jpg', array('mime_type' => 'image/jpeg'));
 
-		return $this->versions;
-	}
+            if (is_wp_error($editor)) {
+                $this->versions['editor'] = false;
+            }
+        }
 
-	private function checkImageEditor() {
-		if ( $this->versions['wp'] && $this->versions['php'] ) {
-			$editor = wp_get_image_editor( __DIR__ . '/rzf-test.jpg', [ 'mime_type' => 'image/jpeg' ] );
+        return $this->versions;
+    }
 
-			if ( is_wp_error( $editor ) ) {
-				$this->versions['editor'] = false;
-			}
-		}
+    public function errors()
+    {
+        return in_array(false, $this->versions, true);
+    }
 
-		return $this->versions;
-	}
+    public function notice()
+    {
+        $notices = array(
+            'php'    => sprintf(
+                __('Resizefly requires at least PHP %s to function properly. Please upgrade PHP to use Resizefly.', 'resizefly'),
+                self::PHP_VERSION
+            ),
+            'wp'     => sprintf(
+                __('Resizefly requires at least WordPress %s to function properly. Please upgrade WordPress to use Resizefly.', 'resizefly'),
+                self::WP_VERSION
+            ),
+            'editor' => __('Resizefly could not find an Image Editor. Please make sure you have either GD or Imagick installed.', 'resizefly'),
+        );
 
-	public function errors() {
-		return in_array( false, $this->versions, true );
-	}
-
-	public function notice() {
-		$notices = [
-			'php'    => sprintf(
-				__( 'Resizefly requires at least PHP %s to function properly. Please upgrade PHP to use Resizefly.',
-					'resizefly'
-				),
-				self::PHP_VERSION
-			),
-			'wp'     => sprintf(
-				__( 'Resizefly requires at least WordPress %s to function properly. Please upgrade WordPress to use Resizefly.',
-					'resizefly'
-				),
-				self::WP_VERSION
-			),
-			'editor' => __( 'Resizefly could not find an Image Editor. Please make sure you have either GD or Imagick installed.',
-				'resizefly'
-			),
-		];
-
-		foreach ( $this->versions as $key => $version ) {
-			if ( ! $version ) {
-				printf( '<div class="error"><p>%s</p></div>', esc_html( $notices[ $key ] ) );
-			}
-		}
-
-	}
-
-	public function deactivate() {
-		deactivate_plugins( plugin_basename( $this->file ), true );
-	}
+        foreach ($this->versions as $key => $version) {
+            if (!$version) {
+                printf('<div class="error"><p>%s</p></div>', esc_html($notices[$key]));
+            }
+        }
+    }
 }
