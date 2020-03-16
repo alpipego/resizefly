273c04d83ee4f9e569baedd56197cb9544783c67
Alex Goller <alpipego@protonmail.com>
2020-03-16 13:28:26 +0700

    save image sizes on plugin activation
    fixes #32

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,6 +5,7 @@
  * Date: 14.07.2017
  * Time: 10:45.
  */
+
 use Alpipego\Resizefly\Admin\Licenses\LicensesPage;
 use Alpipego\Resizefly\Admin\Licenses\LicensesSection;
 use Alpipego\Resizefly\Common\Composer\Autoload\ClassLoader;
@@ -83,7 +84,10 @@ add_action('plugins_loaded', function () use ($classLoader) {
     if (is_admin()) {
         $plugin->addDefiniton(__DIR__.'/config/admin.php');
 
-        require_once __DIR__.'/actions/activation.php';
+        register_activation_hook($file, function () use ($plugin) {
+            require_once __DIR__.'/actions/activation.php';
+        });
+
 
         // add compatibility fixes that are only needed in admin
         $plugin->addDefiniton(__DIR__.'/config/compatibles-admin.php');
