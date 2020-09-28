7a8fdbafc0b9c55e524e6c4e163e009dbf9e1122
Alex Goller <alpipego@protonmail.com>
2020-09-28 15:19:20 +0200

    fix duplicate query

--- resizefly.php
+++ resizefly.php
@@ -4,7 +4,7 @@
  * Plugin Name: Resizefly
  * Description: Dynamically resize your images on the fly
  * Plugin URI:  https://resizefly.com/
- * Version:     3.2.4
+ * Version:     3.2.5
  * Author:      alpipego
  * Author URI:  https://alpipego.com/
  * License:     MIT
