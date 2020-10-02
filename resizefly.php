ef6adba5da91c578127346c3bbb0d8232f46490b
Alex Goller <alpipego@protonmail.com>
2020-10-02 21:59:55 +0200

    bump version to 3.2.6-beta.1

--- resizefly.php
+++ resizefly.php
@@ -4,7 +4,7 @@
  * Plugin Name: Resizefly
  * Description: Dynamically resize your images on the fly
  * Plugin URI:  https://resizefly.com/
- * Version:     3.2.5
+ * Version:     3.2.6-beta.1
  * Author:      alpipego
  * Author URI:  https://alpipego.com/
  * License:     MIT
